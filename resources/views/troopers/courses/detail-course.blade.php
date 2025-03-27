f4b052e124881955e759ea7fcd15667006bbd536
Fio Juliansyah <63538861+fiojuliansyah@users.noreply.github.com>
2025-03-28 03:57:35 +0700

    Update detail-course.blade.php

--- resources/views/troopers/courses/detail-course.blade.php
+++ resources/views/troopers/courses/detail-course.blade.php
@@ -64,48 +64,15 @@
 
         <div class="col-md-4">
             <h4 class="mb-20">Discuss</h4>
-            <div class="card">
+            <div class="card mb-20">
                 <div class="card-body">
-                    <div class="chat-box-item-wrapper overflow-y-auto scroll-sm p-24">
-                        <div class="chat-box-item d-flex align-items-end gap-8">
-                                <img src="/admin/assets/images/thumbs/avatar-img1.png" alt="" class="w-40 h-40 rounded-circle object-fit-cover flex-shrink-0">
-                                <div class="chat-box-item__content">
-                                    <p class="chat-box-item__text p-16 rounded-16 mb-12">Hello Mac</p>
-                                    <p class="chat-box-item__text py-16 px-16 px-lg-4">Lorem ipsum dolor sit amet consectetur. Cursus vulputate eget ullamcorper bibendum pulvinar sed at libero. Vulputate amet fermentum sapien amet tempus ac donec.</p>
-                                    <span class="text-gray-200 text-13 mt-2 d-block">10 min ago</span>
-                                </div>
-                        </div>
-                        <div class="chat-box-item right d-flex align-items-end gap-8">
-                                <img src="/admin/assets/images/thumbs/avatar-img1.png" alt="" class="w-40 h-40 rounded-circle object-fit-cover flex-shrink-0">
-                                <div class="chat-box-item__content">
-                                    <p class="chat-box-item__text py-16 px-16 px-lg-4">Lorem ipsum dolor sit amet consect.Cursus vulputate eget ullamcorper bibendum </p>
-                                    <span class="text-gray-200 text-13 mt-2 d-block">10 min ago</span>
-                                </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-footer border-top border-gray-100">
-                    <form action="#" class="flex-align gap-8 chat-box-bottom">
-                        <label for="fileUp" class="flex-shrink-0 file-btn w-48 h-48 flex-center bg-main-50 text-24 text-main-600 rounded-circle hover-bg-main-100 transition-2">
-                            <i class="ph ph-plus"></i>
-                        </label>
-                        <input type="file" name="fileName" id="fileUp" hidden>
-                        <input type="text" class="form-control h-48 border-transparent px-20 focus-border-main-600 bg-main-50 rounded-pill placeholder-15" placeholder="Type your message...">
-                        <button type="submit" class="flex-shrink-0 submit-btn btn btn-main rounded-pill flex-align gap-4 py-15">
-                            Submit <span class="d-flex text-md d-sm-flex d-none"><i class="ph-fill ph-paper-plane-tilt"></i></span> 
-                        </button>
-                    </form>
+                    <p class="mb-16">Diskusi akan tersedia setelah membeli kelas ini</p>
                 </div>
             </div>
+            <h4 class="mb-20">Materi</h4>
             <div class="card mt-24">
                 <div class="card-body p-0">
                     <div class="course-item">
-                        <button type="button" class="course-item__button active flex-align gap-4 w-100 p-16 border-bottom border-gray-100">
-                            <span class="d-block text-start">
-                                <span class="d-block h5 mb-0 text-line-1">The Courses Program</span>
-                            </span>
-                            <span class="course-item__arrow ms-auto text-20 text-gray-500"><i class="ph ph-arrow-right"></i></span>
-                        </button>
                         <div class="course-item-dropdown active border-bottom border-gray-100">
                             <ul class="course-list p-16 pb-0">
                                 @forelse ($course->videos as $video)
