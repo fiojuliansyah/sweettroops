b422efaa63d7365dbc0525cef8edc89fda198d70
Fio Juliansyah <63538861+fiojuliansyah@users.noreply.github.com>
2025-04-27 19:27:46 +0700

    Update my-detail-course.blade.php

--- resources/views/troopers/courses/my-detail-course.blade.php
+++ resources/views/troopers/courses/my-detail-course.blade.php
@@ -38,7 +38,7 @@
                     <div class="mt-24">
                         <div class="mb-24 pb-24 border-bottom border-gray-100">
                             <h5 class="mb-12 fw-bold">About this course</h5>
-                            <p class="text-gray-300 text-15">...</p>
+                            <p class="text-gray-300 text-15">{!! $course->videos->first()->description !!}</p>
                         </div>
                         <div class="mb-24 pb-24 border-bottom border-gray-100">
                             <h5 class="mb-12 fw-bold">Description</h5>
