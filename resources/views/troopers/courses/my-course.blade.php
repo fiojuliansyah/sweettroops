f1508af36596349cbfc55cf0cfb22715ea3362c3
Fio Juliansyah <63538861+fiojuliansyah@users.noreply.github.com>
2025-04-27 19:04:38 +0700

    Update my-course.blade.php

--- resources/views/troopers/courses/my-course.blade.php
+++ resources/views/troopers/courses/my-course.blade.php
@@ -7,7 +7,7 @@
 <ul class="flex-align gap-4">
 <li><a href="#" class="text-gray-200 fw-normal text-15 hover-text-main-600">Kursus</a></li>
 <li> <span class="text-gray-500 fw-normal d-flex"><i class="ph ph-caret-right"></i></span> </li>
-<li><span class="text-main-600 fw-normal text-15">Semua Kursus</span></li>
+<li><span class="text-main-600 fw-normal text-15">Kursus Saya</span></li>
 </ul>
 </div>
 <!-- Breadcrumb End -->
