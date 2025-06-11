bb29c0e03327e7325144271eb339f36d6e9ece6e
Fio Mcplus <63538861+fiojuliansyah@users.noreply.github.com>
2025-06-12 00:55:47 +0700

    Update my-detail-course.blade.php

--- resources/views/troopers/courses/my-detail-course.blade.php
+++ resources/views/troopers/courses/my-detail-course.blade.php
@@ -33,7 +33,7 @@
 
                     <!-- Video Iframe -->
                     <div class="rounded-16 overflow-hidden" style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%;">
-                        <iframe src="https://drive.google.com/file/d/{{ $videoFileId }}/preview" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" allow="autoplay" frameborder="0"></iframe>
+                        <iframe src="{{ $video->link_url }}" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" allow="autoplay" frameborder="0"></iframe>
                     </div>                                 
                     
                     <div class="mt-24">
