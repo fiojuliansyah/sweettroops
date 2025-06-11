85cc2ae7a72b184b3a88f119297bfacdcd96e6e7
Fio Mcplus <63538861+fiojuliansyah@users.noreply.github.com>
2025-06-12 00:40:04 +0700

    Update my-detail-course.blade.php

--- resources/views/troopers/courses/my-detail-course.blade.php
+++ resources/views/troopers/courses/my-detail-course.blade.php
@@ -32,7 +32,7 @@
                     </div>
 
                     <div class="rounded-16 overflow-hidden" style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%;">
-                        @if ($course->videos->first()->type == 'video')
+                        @if ($course->videos->first()->type == 'url')
                             <iframe src="https://drive.google.com/file/d/{{ $videoFileId }}/preview" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" allow="autoplay" frameborder="0"></iframe>
                         @else 
                             <iframe src="{{ $course->videos->first()->link_url }}"  style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"  allow="autoplay" frameborder="0"></iframe>
