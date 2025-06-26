2ca6a6cc083b28b4d977fe3ca11583102492ca37
Fio Mcplus <63538861+fiojuliansyah@users.noreply.github.com>
2025-06-26 11:47:19 +0700

    update

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'manage/videos/upload', // Add this line to exclude your upload endpoint
+        'manage/videos/upload',
+        'api/midtrans-callback',
     ];
 }
