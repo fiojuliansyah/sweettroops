a9f9573e64c4405108689827b2b7632a1e8b19f4
Fio Juliansyah <63538861+fiojuliansyah@users.noreply.github.com>
2025-09-11 14:03:59 +0700

    Update course-detail.blade.php

--- resources/views/course-detail.blade.php
+++ resources/views/course-detail.blade.php
@@ -104,8 +104,15 @@
     </div>
 </section>
 <section class="section-44">
-    <div class="w-layout-blockcontainer container-42 w-container"><a href="javascript:void(0)"
-            class="button-5 w-button buy-course" data-course-id="{{ $course->id }}">ENROLL</a></div>
+    <div class="w-layout-blockcontainer container-42 w-container">
+        @auth 
+            <a href="javascript:void(0)" class="button-5 w-button buy-course" data-course-id="{{ $course->id }}">ENROLL</a>
+        @endauth
+
+        @guest
+            <a href="{{ route('login-first') }}" class="button-5 w-button">ENROLL</a>
+        @endguest
+    </div>
 </section>
 <section class="section-43">
     <div class="w-layout-blockcontainer container-41 w-container">
